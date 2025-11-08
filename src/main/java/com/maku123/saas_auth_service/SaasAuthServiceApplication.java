c419f8f01c48df433e0b77375d16ae2f2a943b9a
Manish Kumar <lit2019044@iiitl.ac.in>
2025-11-08 23:32:57 +0530

    first commit

---
+++ src/main/java/com/maku123/saas_auth_service/SaasAuthServiceApplication.java
@@ -0,0 +1,13 @@
+package com.maku123.saas_auth_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SaasAuthServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SaasAuthServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/maku123/saas_auth_service/SaasAuthServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.maku123.saas_auth_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SaasAuthServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
